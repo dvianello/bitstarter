d1b01688e308f009fb0c5f746b686e74780a4b4c
Dario Vianello <vianellod@tin.it>
2013-07-04 21:33:20 +0000

    First index.html web.js test

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
+var fs = require('fs')
+var buf = require('buffer')
+
+var infile = 'index.html'
+
+var index_text = fs.readFileSync(infile).toString()
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2
-!');
+  response.send(index_text);
 });
 
 var port = process.env.PORT || 5000;
